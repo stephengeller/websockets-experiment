5a0e2c2ceb71b8f99f6bd01e7abd22dc9296213f
Stephen Geller <stephen.geller@hotmail.com>
2018-09-15 11:19:27 -0400

    feat(mongo-socket): Add disconnect registering in server.

--- socket-io-mongodb/index.js
+++ socket-io-mongodb/index.js
@@ -1,62 +1,80 @@
 const express = require("express");
 const socket = require("socket.io");
-const mongo = require('mongodb').MongoClient;
+const mongo = require("mongodb").MongoClient;
 const PORT = process.env.PORT || 4000;
 const app = express();
-const server = app.listen(PORT, function() {
-    console.log(`Running on port ${PORT}`);
-});
-const io = socket(server);
 const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASS = process.env.MONGO_PASS;
-const MONGO_URI=""
+
+const server = app.listen(PORT, function() {
+  console.log(`Running on port ${PORT}`);
+});
+const io = socket(server);
 
 console.log(MONGO_USER, MONGO_PASS);
 
 app.use(express.static("public"));
 
-mongo.connect(`mongodb://${MONGO_USER}:${MONGO_PASS}@ds245687.mlab.com:45687/chatroom-db`, {
-  useNewUrlParser: true
-}, (err, client) => {
-  if (err) throw err;
+mongo.connect(
+  `mongodb://${MONGO_USER}:${MONGO_PASS}@ds245687.mlab.com:45687/chatroom-db`,
+  {
+    useNewUrlParser: true
+  },
+  (err, client) => {
+    if (err) throw err;
 
-  console.log('Mongodb connected...');
+    console.log("Mongodb connected...");
 
-  io.on("connection", socket => {
-    const db = client.db('chatroom-db');
-    const chat = db.collection('chats');
+    const connections = [];
 
-    const sendStatus = (s) => {
-      socket.emit('status', s)
-    };
-    // chat.
-    chat.find({}).limit(100).sort({_id:1}).toArray((err, res) => {
-      if (err) throw err;
-      socket.emit('output', res)
-    });
+    io.on("connection", socket => {
+      connections.push(socket);
+      console.log(`${connections.length} number of connections.`);
 
-    socket.on('input', (data) => {
+      const db = client.db("chatroom-db");
+      const chat = db.collection("chats");
+
+      const sendStatus = s => {
+        socket.emit("status", s);
+      };
+      // chat.
+      chat
+        .find({})
+        .limit(100)
+        .sort({ _id: 1 })
+        .toArray((err, res) => {
+          if (err) throw err;
+          socket.emit("output", res);
+        });
+
+      socket.on("input", data => {
         let name = data.name;
         let message = data.message;
 
-        if (name === '' || message === '') {
-            sendStatus('Please enter a name and message')
+        if (name === "" || message === "") {
+          sendStatus("Please enter a name and message");
         } else {
-            chat.insertOne({name, message}, () => {
-                console.log(data);
-                io.emit('output', [data]);
-                sendStatus({
-                    message: 'Message sent',
-                    clear: true
-                })
-            })
+          chat.insertOne({ name, message }, () => {
+            console.log(data);
+            io.emit("output", [data]);
+            sendStatus({
+              message: "Message sent",
+              clear: true
+            });
+          });
         }
-    })
+      });
 
-    socket.on('clear', () => {
+      socket.on("clear", () => {
         chat.removeMany({}, () => {
-            socket.emit('cleared')
-        })
-    })
-  });
-});
+          socket.emit("cleared");
+        });
+      });
+
+      socket.on("disconnect", () => {
+          connections.splice(connections.indexOf(socket), 1);
+          console.log(`${connections.length} number of connections.`);
+      })
+    });
+  }
+);
--- socket-io-mongodb/public/chat.js
+++ socket-io-mongodb/public/chat.js
@@ -1,67 +1,67 @@
-const element = (id) => {
-    return document.getElementById(id)
+const element = id => {
+  return document.getElementById(id);
 };
 
-const status = element('status');
-const messages = element('messages');
-const textarea = element('textarea');
-const username = element('username');
-const clearBtn = element('clear');
+const status = element("status");
+const messages = element("messages");
+const textarea = element("textarea");
+const username = element("username");
+const clearBtn = element("clear");
 
 const statusDefault = status.textContent;
 
-const setStatus = (s) => {
-    status.textContent = s;
+const setStatus = s => {
+  status.textContent = s;
 
-    if (s !== statusDefault) {
-        const delay = setTimeout(() => setStatus(statusDefault), 4000)
-    }
+  if (s !== statusDefault) {
+    const delay = setTimeout(() => setStatus(statusDefault), 4000);
+  }
 };
 
 const socket = io.connect();
 
 if (socket !== undefined) {
-    console.log('connected to socket...');
+  console.log("connected to socket...");
+  socket.on("output", function(data) {
+    if (data.length) {
+      for (let x = 0; x < data.length; x++) {
+        // Build out message div
+        const message = document.createElement("div");
+        message.setAttribute("class", "chat-message");
+        message.innerHTML = `<strong>${data[x].name}</strong>: ${
+          data[x].message
+        }`;
+        messages.appendChild(message);
+        messages.insertBefore(message, messages.firstChild);
+      }
+    }
+  });
 
-    socket.on('output', function(data) {
-        console.log(data);
-        if (data.length) {
-            for(let x = 0;x < data.length;x++){
-                // Build out message div
-                const message = document.createElement('div');
-                message.setAttribute('class', 'chat-message');
-                message.innerHTML = `<strong>${data[x].name}</strong>: ${data[x].message}`;
-                messages.appendChild(message);
-                messages.insertBefore(message, messages.firstChild);
-            }
-        }
-    })
+  socket.on("status", data => {
+    setStatus(typeof data === "object" ? data.message : data);
 
-    socket.on('status', (data) => {
-        setStatus(typeof data === 'object' ? data.message : data);
+    if (data.clear) {
+      textarea.value = "";
+    }
+  });
 
-        if (data.clear) {
-            textarea.value = ''
-        }
-    })
+  textarea.addEventListener("keydown", e => {
+    // event 13 is enter and not holding enter
+    if (e.which === 13 && e.shiftKey === false) {
+      socket.emit("input", {
+        name: username.value,
+        message: textarea.value
+      });
 
-    textarea.addEventListener('keydown', (e) => {
-        // event 13 is enter and not holding enter
-        if(e.which === 13 && e.shiftKey === false) {
-            socket.emit('input', {
-                name: username.value,
-                message: textarea.value
-            });
+      event.preventDefault();
+    }
+  });
 
-            event.preventDefault();
-        }
-    })
-
-    clearBtn.addEventListener('click', function(){
-        socket.emit('clear');
-    });
-    // Clear Message
-    socket.on('cleared', function(){
-        messages.textContent = '';
-    });
+  clearBtn.addEventListener("click", function() {
+    socket.emit("clear");
+  });
+  // Clear Message
+  socket.on("cleared", function() {
+    messages.textContent = "";
+  });
 }
